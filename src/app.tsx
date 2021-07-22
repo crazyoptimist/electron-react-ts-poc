f38a64e501deb3ebdd0ae8c4d73c4b8f7fa03676
crazyoptimist <crazyoptimist@mail.com>
2021-07-21 20:09:56 -0500

    add a simple component

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import HelloWidget from './components/hello-widget/hello-widget.component';
+
+function render() {
+  ReactDOM.render(<HelloWidget />, document.getElementById("root"));
+}
+
+render();
---
+++ src/components/hello-widget/hello-widget.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './hello-widget.styles.css';
+
+
+const HelloWidget: React.FC = () => {
+  return <p>Hello, world!</p>
+}
+
+export default HelloWidget;
